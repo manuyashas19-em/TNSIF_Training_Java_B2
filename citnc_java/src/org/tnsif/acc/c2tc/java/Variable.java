a41fd7a3a5166d0b32fe63f0614f8a609e8a0bb9
Manu yashas EM <Manu yashas EM@ManuYashas>
2026-07-16 12:38:23 +0530

    first commit

--- citnc_java/src/org/tnsif/acc/c2tc/java/Variable.java
+++ citnc_java/src/org/tnsif/acc/c2tc/java/Variable.java
@@ -1,10 +1,29 @@
 package org.tnsif.acc.c2tc.java;
 
 public class Variable {
+    // Instance variable (belongs to each object)
+    int price = 10;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Static variable (shared across all objects)
+    static String category = "Electronics";
 
-	}
+    // Method with a local variable
+    void print() {
+        String msg = "hello"; // local variable
+        System.out.println(msg);
+    }
 
+    public static void main(String[] args) {
+        // Create an object of Variable class
+        Variable v = new Variable();
+
+        // Access instance variable
+        System.out.println("Instance variable price: " + v.price);
+
+        // Access static variable
+        System.out.println("Static variable category: " + Variable.category);
+
+        // Call method to print local variable
+        v.print();
+    }
 }
